946e73860f1bbc23c26a70f888234ace9ee27a25
Mohamed Ouazane <mohamedouazane@yahoo.com>
2023-07-22 06:21:47 -0700

    adding printing_fun

---
+++ printing_fun.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_string - Print a string.
+ *@arg: list of arguments.
+ * return: Number of chars printed.
+ */
+int print_string(va_list arg)
+{
+
+}
+/**
+ * print_c - Print a char.
+ *@arg: list of arguments.
+ * return: Number of chars printed.
+ */
+int print_c(va_list arg)
+{
+
+}
+/**
+ * print_percent - Print a percent.
+ *@arg: list of arguments.
+ * return: Number of chars printed.
+ */
+int print_percent(va_list arg)
+{
+
+}
+
